e65b95b4ebf60edfd74b9571da6159b7f738ea47
João Schefer <joaogfschefer2121@gmail.com>
2023-05-24 21:58:40 -0300

    git dos comentarios

--- teste.py
+++ teste.py
@@ -1,1 +1,11 @@
+# COMANDOS PARA COLOCAR O CÓDIGO NO GITHUB #
+# Abrir a pasta do projeto no vscode e abrir o terminal
+# Inicializar o repositório git = git init
+# Adicionar todos os arquivos = git add . ou git add "nome do arquivo"
+# Confirmar alterações = git commit -m "Nome do commit"
+# Adicionar a URL do repositório = git remote add origin <URL_DO_REPOSITÓRIO>
+# Enviar as alterações para o github = git push -u origin master
+
+
+
 print("Opa")
